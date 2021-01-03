eca99a4c4c29488c5ab17a6005e7083a0dd2af61
Robert Li <robertwli@gmail.com>
2021-01-03 18:19:52 +1100

    fixed up urls and index home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,61 +4,42 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import photos from "./photos"
+import { Col, Row } from "react-bootstrap"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
 
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <SEO title="Home" />
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  }
-
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="Home" />
-      <Bio />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
 
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
+      <section>
+
+        <Row>
+          <Col xs={4}>
+            <Link to="/blog">Blog</Link>
+            <p>I write about various things. More recently about technology and business.</p>
+          </Col>
+          <Col xs={4}>
+            <Link to="/photos">Photos</Link>
+            <p>I first learned to code because I couldn't find photo hosting with gallery templates to my liking.</p>
+          </Col>
+          <Col xs={4}>
+            <Link to="/about">About me</Link>
+            <p>My past and present projects.</p>
+            <Bio />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={4}>
+            <Link to="https://github.com/itsrobli">Code</Link>
+            <p>I've recently started to open source some stuff.</p>
+          </Col>
+        </Row>
+      </section>
+
     </Layout>
   )
 }
