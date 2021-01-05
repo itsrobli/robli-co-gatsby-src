92ac0fdb7d86d36e7005a1da18830917717acf12
Robert Li <robertwli@gmail.com>
2021-01-06 02:24:07 +1100

    about page coming along

--- src/components/bio.js
+++ src/components/bio.js
@@ -8,13 +8,21 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
+import { Col, Row } from "react-bootstrap"
+// import { FaGithub, FaTwitter, FaInstagram } from "@react-icons/all-files"
+import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin"
+import { FaGithub } from "@react-icons/all-files/fa/FaGithub"
+import { FaTwitter } from "@react-icons/all-files/fa/FaTwitter"
+import { FaInstagram } from "@react-icons/all-files/fa/FaInstagram"
+
+
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
       avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
         childImageSharp {
-          fixed(width: 50, height: 50, quality: 95) {
+          fixed(width:80, height: 80, quality: 95) {
             ...GatsbyImageSharpFixed
           }
         }
@@ -41,26 +49,29 @@ const Bio = () => {
 
   return (
     <div className="bio">
-      {avatar && (
-        <Image
-          fixed={avatar}
-          alt={author?.name || ``}
-          className="bio-avatar"
-          imgStyle={{
-            borderRadius: `50%`,
-          }}
-        />
-      )}
-      {author?.name && (
-        <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            Follow on Twitter
-          </a>
-        </p>
-      )}
+      <Row>
+        <Col xs={8}>
+          <p>{author?.summary || null}</p>
+          <p>You can find me on: <a href="https://github.com/itsrobli"><FaGithub/></a> <a
+            href="https://www.linkedin.com/in/itsrobli/"><FaLinkedin/></a> <a
+            href={`https://twitter.com/${social?.twitter || ``}`}><FaTwitter/></a> <a
+            href="https://www.instagram.com/itsrobli/"><FaInstagram/></a></p>
+        </Col>
+        <Col xs={4}>
+          {avatar && (
+            <Image
+              fixed={avatar}
+              alt={author?.name || ``}
+              className="bio-avatar"
+              imgStyle={{
+                borderRadius: `50%`,
+              }}
+            />
+          )}
+        </Col>
+      </Row>
     </div>
+
   )
 }
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -10,19 +10,17 @@ import { Col, Row } from "react-bootstrap"
 import Layout from "../components/layout"
 import Image from "gatsby-image"
 import SEO from "../components/seo"
+import AboutMarkdown from "../../content/assets/about.mdx"
 
 
 const About = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
-  const photosManifest = data.allMdx.nodes
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="About" />
-      {/*<div style={{ maxWidth: `960px`, margin: `1.45rem` }}>*/}
       <TitleHeader>About</TitleHeader>
+      <AboutMarkdown />
 
-
-      {/*</div>*/}
     </Layout>
   )
 }
@@ -34,28 +32,6 @@ export const pageQuery = graphql`
         site {
             siteMetadata {
                 title
-            }
-        }
-        allMdx(filter: {fileAbsolutePath: {regex: "/content/photos/"}}, sort: {fields: [frontmatter___id, frontmatter___date], order: ASC}) {
-            nodes {
-                excerpt
-                fields {
-                    slug
-                }
-                frontmatter {
-                    date(formatString: "MMMM DD, YYYY")
-                    title
-                    description
-                    id
-                    category
-                    thumbnail {
-                        childImageSharp {
-                            fluid {
-                                ...GatsbyImageSharpFluid
-                            }
-                        }
-                    }
-                }
             }
         }
     }
